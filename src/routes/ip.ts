c685c9aa3e568ab447714799d77f6667402d4c6a
Rob Holme <rob@holme.com.au>
2025-02-20 13:56:54 +0800

    copy of cloud provider JSON files

--- src/routes/ip.ts
+++ src/routes/ip.ts
@@ -1,26 +1,46 @@
 import { Router, Request, Response } from "express";
 import { TestIPv4Address, TestPrivateAddress, TestIpInSubnet } from '../util/ip-utils';
-import {GetCloudProviderSubnets} from '../util/cloudprovider-utils'
+import { GetCloudProviderSubnets, cloudProviderJSON } from '../util/cloudprovider-utils'
 const router = Router();
 
+// -----------------------------
 // Get IP address router
 // Use a path parameter to retrieve the IP address to query. e.g.  http://server/ip/20.340.54.4
+// -----------------------------
 router.get("/:ip", (req: Request, res: Response) => {
-    const ipAddress = req.params.ip;
-    // return error if the string does not match the format of an IPv4 address
-    if (TestIPv4Address(ipAddress)) {
-      // return an error for private addresses - likely to be private endpoints
-      if (TestPrivateAddress(ipAddress) == true) {
-        res.status(404).json({ message: "IPv4 Address is a reserved address" });
-        return;
-      }
-      // get the Azure subnets, filtered on the first octet of the IP Address
-      var azureSubnets : JSON = GetCloudProviderSubnets('./src/cloudproviders/Azure.json',ipAddress.split(".")[0]);
-      res.send(azureSubnets);
-      //res.send(TestIpInSubnet(ipAddress, "20.0.0.0/24"));
-    } else {
-      res.status(404).json({ message: "IPv4 Address failed validation" });
-    }
-  });
+  const ipAddress = req.params.ip;
+  //var jsonResult : JSON = JSON.parse("[]");
+  var jsonResult : Object[] = [];
+  res.setHeader('content-type', 'application/json');
 
-  export default router;
+  // return error if the string does not match the format of an IPv4 address
+  if (TestIPv4Address(ipAddress)) {
+    // return an error for private addresses - likely to be private endpoints
+    if (TestPrivateAddress(ipAddress) == true) {
+      res.status(404).json({ message: "IPv4 Address is a reserved address" });
+      return;
+    }
+    // get the cloud provider subnets (and region/service), filtered on the first octet of the IP Address matching the start of the subnet network address 
+    var CloudProviderDetails: cloudProviderJSON[] = GetCloudProviderSubnets('./src/cloudproviders/Azure.json', ipAddress.split(".")[0]);
+    CloudProviderDetails.push(...GetCloudProviderSubnets('./src/cloudproviders/AWS.json', ipAddress.split(".")[0]));
+    CloudProviderDetails.push(...GetCloudProviderSubnets('./src/cloudproviders/AWS.json', ipAddress.split(".")[0]));
+    CloudProviderDetails.push(...GetCloudProviderSubnets('./src/cloudproviders/Azure.json', ipAddress.split(".")[0]));
+    CloudProviderDetails.push(...GetCloudProviderSubnets('./src/cloudproviders/GoogleCloud.json', ipAddress.split(".")[0]));
+    CloudProviderDetails.push(...GetCloudProviderSubnets('./src/cloudproviders/OCI.json', ipAddress.split(".")[0]));
+    CloudProviderDetails.push(...GetCloudProviderSubnets('./src/cloudproviders/Akamai.json', ipAddress.split(".")[0]));
+    CloudProviderDetails.push(...GetCloudProviderSubnets('./src/cloudproviders/CloudFlare.json', ipAddress.split(".")[0]));
+
+    CloudProviderDetails.forEach((currentSubnet: cloudProviderJSON) => {
+      if (TestIpInSubnet(ipAddress, currentSubnet.ip_prefix)) {
+        jsonResult.push(currentSubnet);
+      }
+    });
+
+    res.send(JSON.stringify(jsonResult, null, 2));
+    //res.send(TestIpInSubnet(ipAddress, "20.0.0.0/24"));
+  } else {
+    res.status(404).json({ message: "IPv4 Address failed validation" });
+  }
+});
+
+export default router;
