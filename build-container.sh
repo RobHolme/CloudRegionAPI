29fe4270a247007bfab1a248db0cfb2a498d1aaf
Rob Holme <rob@holme.com.au>
2025-03-09 09:48:13 +0800

    commended out pwsh script to update JSON files - now down via github action

--- build-container.sh
+++ build-container.sh
@@ -1,3 +1,10 @@
- pwsh -File ./update-cloudproviderjson.ps1
- sudo node --run build
- sudo docker build -t robholme/cloud-region-api .
+# build a local container. 
+
+# Optional - uncomment next line to update JSON cloud provider definitions. Not required as files are updated weekly via GitHub action. Requires Powershell > v7
+# pwsh -File ./update-cloudproviderjson.ps1
+
+# build the node solution
+sudo node --run build
+
+# build the local container
+sudo docker build -t robholme/cloud-region-api .
