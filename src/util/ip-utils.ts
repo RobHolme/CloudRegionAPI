86ca25c080d7ee94cae035a5c9b82a946ad6654d
Rob Holme <rob@holme.com.au>
2025-03-17 21:04:12 +0800

    added function to resolve ipv4 addresses

--- src/util/ip-utils.ts
+++ src/util/ip-utils.ts
@@ -1,10 +1,12 @@
+import dns from 'node:dns';
+
 //-----------------------------
 // Function:    TestIPv4Address
 // Description: Return true if the parameter matches the pattern of an IPv4 Address  
 //-----------------------------
-export function TestIPv4Address(IpAddress: string) : boolean {
+export function TestIPv4Address(IpAddress: string): boolean {
     var ip4RegEx: RegExp = new RegExp('^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
-	
+
     if (ip4RegEx.test(IpAddress)) {
         return true;
     }
@@ -15,9 +17,9 @@ export function TestIPv4Address(IpAddress: string) : boolean {
 // Function:    TestIPv4Subnet
 // Description: Return true if the parameter matches the pattern of an IPv4 subnet (CIDR notation)  
 //-----------------------------
-export function TestIPv4Subnet(IpSubnet: string) : boolean {
+export function TestIPv4Subnet(IpSubnet: string): boolean {
     var ip4SubnetRegEx: RegExp = new RegExp('^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/[0-9]{1,2}$')
-	
+
     if (ip4SubnetRegEx.test(IpSubnet)) {
         return true;
     }
@@ -28,17 +30,17 @@ export function TestIPv4Subnet(IpSubnet: string) : boolean {
 // Function:    TestPrivateAddress
 // Description: Return true if IpAddress is within the rfc1918 private address range
 //-----------------------------
-export function TestPrivateAddress (IpAddress: string) : boolean {
+export function TestPrivateAddress(IpAddress: string): boolean {
 
-    var reservedAddressRanges : string[] = [
-        "172.16.0.0/12", 
-        "10.0.0.0/8", 
+    var reservedAddressRanges: string[] = [
+        "172.16.0.0/12",
+        "10.0.0.0/8",
         "192.168.0.0/16",
         "100.64.0.0/10",
         "127.0.0.0/8",
         "169.254.0.0/16"
     ]
-    
+
     for (var i = 0; i < reservedAddressRanges.length; i++) {
         if (TestIpInSubnet(IpAddress, reservedAddressRanges[i])) {
             return true;
@@ -52,14 +54,14 @@ export function TestPrivateAddress (IpAddress: string) : boolean {
 // Function:    TestIpInSubnet
 // Description: Return true if the IP address falls within the Subnet
 //-----------------------------
-export function TestIpInSubnet (IpAddress: string, Subnet: string) : boolean {
-    
+export function TestIpInSubnet(IpAddress: string, Subnet: string): boolean {
+
     const [subnetAddress, subnetMaskLength] = Subnet.split('/');
     const subnetMask = ~((1 << (32 - parseInt(subnetMaskLength, 10))) - 1) >>> 0;
     const ipLong = IpToLong(IpAddress);
     const subnetLong = IpToLong(subnetAddress);
 
-    return (ipLong & subnetMask) === (subnetLong & subnetMask); 
+    return (ipLong & subnetMask) === (subnetLong & subnetMask);
 
 }
 
@@ -70,3 +72,17 @@ function IpToLong(ip: string): number {
     return ip.split('.').reduce((acc, octet) => (acc << 8) + parseInt(octet, 10), 0) >>> 0;
 }
 
+//-----------------------------
+// Function:    resolveIPv4Addresses
+// Description: Resolve the IPv4 Addresses for a given hostname 
+export function resolveIPv4Addresses(hostname: string): Promise<string[] | null> {
+    return new Promise((resolve, err) => {
+        dns.resolve4(hostname, (err, addresses) => {
+            if (err) {
+                resolve(null);
+            } else {
+                resolve(addresses);
+            }
+        });
+    });
+}
