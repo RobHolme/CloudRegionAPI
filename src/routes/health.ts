0fe9110d750bae713dea365ee64c22e3d2e04f3a
Rob Holme <rob@holme.com.au>
2025-11-07 09:11:50 +0800

    added content-type response header

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -5,6 +5,7 @@ const router = Router();
 // Health check URL for Docker health check
 // -----------------------------
 router.get('/', (req, res) => {
+    res.setHeader('content-type', 'application/json');
     res.status(200).json({ message: 'healthy' });
 });
 
